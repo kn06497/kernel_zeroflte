1b6a8f9a4f7b935f0a9c327a0829af2a577f4c77
kn06497 <kn06497@hotmail.com>
2017-10-11 14:50:59 +0000

    update build script
    Signed-off-by: kn06497 <kn06497@hotmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,5 @@
-
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-
-make zeroflte_02_defconfig
-make -j
+export CROSS_COMPILE=/home/knoth06/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+mkdir output
+make -C $(pwd) O=output  zeroflte_02_defconfig
+make -j64 -C $(pwd) O=output
